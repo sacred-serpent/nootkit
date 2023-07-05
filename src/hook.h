eb0b9aefec6211de615d19b9ec70944fb8d53d71
serpent <sacred-serpent@users.noreply.github.com>
2023-07-05 20:00:05 +0300

    hook: fix: hook_set Documentation Error
    A part was erroneously left from a previous commit.

--- src/hook.h
+++ src/hook.h
@@ -5,17 +5,12 @@
 #include "common.h"
 
 /// @brief Replace the first bytes at `addr` with a jmp gadget referring to the 64 bit address `hook`.
-/// 
-/// Sets `jmp_gadget` and `hook_last_replaced` to the changed bytes,
-/// allowing to save them for later hook unsetting.
-///
-/// This mechanism leaves the target function unusable outside of jumping
-/// to the hook, until the hook is unset.
-/// 
+/// @details This mechanism leaves the target function unusable outside of jumping
+///     to the hook, until the hook is unset.
 /// @param addr address to set hook at
 /// @param hook address of function to act as a hook
 /// @returns `struct view` pointing to a byte array which can be used to unset the hook.
-///     WARNING: the data pointed to by the returned view may change on subsequent hook_set_* calls,
+/// @warning The data pointed to by the returned view may change on subsequent hook_set_* calls,
 ///     so it should be saved externally before any other hooks are set using this method.
 struct view hook_set(void *addr, void *hook);
 
